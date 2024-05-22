2a3f0f39582a3c52df47a22f1eaf0f31dec1b190
Henriquesagoncalves <167038326+Henriquesagoncalves@users.noreply.github.com>
2024-05-21 21:42:27 -0300

    Corrigindo menu
    realizando a correção dos espaços entre as opções do menu

--- cartorio.c
+++ cartorio.c
@@ -126,7 +126,7 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
 		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: ");//fim do menu
 	
